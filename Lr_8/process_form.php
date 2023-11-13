0fbc9b064b6a393f87c3467f286ef92416d7840e
Nicolay421 <yarmolovich.nicolay@gmail.com>
2023-11-13 09:47:19 +0200

    змінив process_form

--- Lr_8/process_form.php
+++ Lr_8/process_form.php
@@ -1,16 +1,32 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $xml = simplexml_load_file('book.xml');
-
-    foreach ($xml->book as $book) {
-        $book->title = $_POST['title'];
-        $book->author = $_POST['author'];
-        $book->publisher = $_POST['publisher'];
-        $book->publication_date = $_POST['publication_date'];
+    $required_fields = ['book_id', 'title', 'author', 'publisher', 'publication_date'];
+    foreach ($required_fields as $field) {
+        if (!isset($_POST[$field])) {
+            die("Ошибка: Не все необходимые поля предоставлены.");
+        }
     }
 
-    $xml->asXML('book.xml');
+    $xml = simplexml_load_file('book.xml');
+    $book_id_to_update = (string)$_POST['book_id'];
 
-    header('Location: index.php');
+    $bookToUpdate = null;
+    foreach ($xml->book as $book) {
+        if ((string)$book['id'] === $book_id_to_update) {
+            $bookToUpdate = $book;
+            break;
+        }
+    }
+    if ($bookToUpdate !== null) {
+        $bookToUpdate->title = $_POST['title'];
+        $bookToUpdate->author = $_POST['author'];
+        $bookToUpdate->publisher = $_POST['publisher'];
+        $bookToUpdate->publication_date = $_POST['publication_date'];
+        $xml->asXML('book.xml');
+        header('Location: index.php');
+        exit();
+    } else {
+        die("Ошибка: Книга с ID $book_id_to_update не найдена.");
+    }
 }
 ?>
