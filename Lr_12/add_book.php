58f6945ffd9b6f9100aabd501c30ae405419f0b7
Nicolay421 <yarmolovich.nicolay@gmail.com>
2023-11-20 16:26:15 +0200

    Змінив Lr_12

--- Lr_12/add_book.php
+++ Lr_12/add_book.php
@@ -1,34 +1,44 @@
 <?php
-require_once 'config.php'; 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "books";
 
-if ($_SERVER["REQUEST_METHOD"] == "POST"){
-    $dsn = "mysql:host=localhost;dbname=books";
-    $username = "root";
-    $password = "";
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // встановлюємо режим помилок PDO на виключення винятків
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    $options = [
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ];
-
-    try {
-        $conn = new PDO($dsn, $username, $password, $options);
-    } catch (PDOException $e) {
-        die("Помилка підключення: " . $e->getMessage());
-    }
-
-    if(isset($_POST['title'], $_POST['author'], $_POST['published_year'])){
+    if (isset($_POST['title'])) {
         $title = $_POST['title'];
-        $author = $_POST['author'];
-        $published_year = $_POST['published_year'];
-
-        $sql = "INSERT INTO books (title, author, published_year) VALUES (?, ?, ?)";
-        $stmt = $conn->prepare($sql);
-        $stmt->execute([$title, $author, $published_year]);
-
-        echo "Книга занесена";
     } else {
-        echo "Помилка";
+        $title = '';
     }
+
+    if (isset($_POST['author'])) {
+        $author = $_POST['author'];
+    } else {
+        $author = '';
+    }
+
+    if (isset($_POST['published_year'])) {
+        $published_year = $_POST['published_year'];
+    } else {
+        $published_year = '';
+    }
+
+    $sql = "INSERT INTO books (title, author, published_year) VALUES (:title, :author, :published_year)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':title', $title);
+    $stmt->bindParam(':author', $author);
+    $stmt->bindParam(':published_year', $published_year);
+
+    $stmt->execute();
+
+    echo "Книга успішно додана";
+} catch (PDOException $e) {
+    echo "Помилка: " . $e->getMessage();
 }
+
+$conn = null;
 ?>
--- Lr_12/config.php
+++ Lr_12/config.php
@@ -1,13 +1,28 @@
 <?php
-$host = 'localhost';
-$db_name = 'books';
-$username = 'root'; 
-$password = ''; 
+header('Content-Type: text/html; charset=UTF-8');
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$db_name", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch(PDOException $e) {
-    echo "Помилка підключення: " . $e->getMessage();
+$database_host = 'localhost';
+$database_username = 'root';
+$database_password = '';
+$database_name = 'Products';
+
+$mysqli = new mysqli($database_host, $database_username, $database_password, $database_name);
+
+if ($mysqli->connect_error) {
+    die('Error : (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+
+$product_name = '52 inch TV';
+$product_code = '9879798';
+$find_id = 24;
+
+$query = "UPDATE products SET product_name=?, product_code=? WHERE ID=?";
+$statement = $mysqli->prepare($query);
+$results = $statement->bind_param('ssi', $product_name, $product_code, $find_id);
+
+if ($results) {
+    print 'Запит успішно виконаний! Запис оновлено';
+} else {
+    print 'Помилка: (' . $mysqli->errno . ') ' . $mysqli->error;
 }
 ?>
