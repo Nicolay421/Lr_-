fb045d42ae6c9358b5201fc5298fe404bde8bbb2
Nicolay421 <yarmolovich.nicolay@gmail.com>
2023-11-20 12:10:31 +0200

    Змінив Lr__13

--- Lr__13/pogoda.php
+++ Lr__13/pogoda.php
@@ -1,6 +1,6 @@
 <?php
 include "WebPageScraper.php";
-$url = 'https://weather.com/';
+$url = 'https://weather.com/weather/today/l/8d61fe0298405a2caf36ceadc442079a41a66bc97c8b3fd49f0641ac93ad8878';
 $scraper = new WebPageScraper($url);
 $scraper->loadPage();
 
